a1ad6c3be129c232d646e3093cb5dd83d5803460
Victor Guimarães <123411019+Areghor@users.noreply.github.com>
2024-06-15 11:16:37 -0300

    Update biblioteca.sql

--- INSERÇÃOPHP - VANESSA/biblioteca.sql
+++ INSERÇÃOPHP - VANESSA/biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21-Jun-2023 às 02:22
+-- Tempo de geração: 15-Jun-2024 às 16:11
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 8.2.0
 
@@ -32,6 +32,35 @@ CREATE TABLE `autores` (
   `nome_autor` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Extraindo dados da tabela `autores`
+--
+
+INSERT INTO `autores` (`id_autor`, `nome_autor`) VALUES
+(1, 'VICTOR'),
+(2, 'VICTOR'),
+(5, 'Victor'),
+(8, 'FELIPE');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `contatos`
+--
+
+CREATE TABLE `contatos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `numero` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `contatos`
+--
+
+INSERT INTO `contatos` (`id`, `nome`, `numero`) VALUES
+(4, 'Victor Hugo Souza Guimarães', 'TI');
+
 -- --------------------------------------------------------
 
 --
@@ -44,6 +73,13 @@ CREATE TABLE `livro` (
   `data_disponibilidade` datetime NOT NULL,
   `id_autor` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `livro`
+--
+
+INSERT INTO `livro` (`id_livro`, `nome_livro`, `data_disponibilidade`, `id_autor`) VALUES
+(39, 'CARROS', '2024-06-15 00:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -64,7 +100,7 @@ CREATE TABLE `livros` (
 --
 
 INSERT INTO `livros` (`idloc`, `locatario`, `nomelivro`, `idlivro`, `created_at`) VALUES
-(1, 'Victor Hugo', 'Pequeno Principe', 113, '2022-11-07 15:45:20');
+(11, 'Gabriel', 'PEQUENO PRINCIPE', 3, '2024-06-14 20:19:04');
 
 -- --------------------------------------------------------
 
@@ -86,7 +122,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
 (1, 'ptacca', '$2y$10$t/TqRpVq2.2mBNMPVwQXYeVsDgatVDE1et6TElahnLv025dO5vTKi', '2022-11-07 16:01:41'),
 (2, 'teste', '$2y$10$bfWAHcULuDgSBxBaab4IMeoYJUPXAZZgv86/.IyZ22NjXpNY5nzkW', '2022-11-07 17:05:02'),
-(8, 'victor', '$2y$10$Myqb/HbnJm1m.agTtiLCjuinl00Ukwex5ZErR/lX5h4vB..IJfx9O', '2023-06-20 13:50:00');
+(8, 'victor', '$2y$10$Myqb/HbnJm1m.agTtiLCjuinl00Ukwex5ZErR/lX5h4vB..IJfx9O', '2023-06-20 13:50:00'),
+(9, 'vitin', '$2y$10$r6t71.OCrnU14EFcQZyDue7nkLNv.Ubt65az.4ihYtZtnF24dr/Fa', '2023-12-03 12:35:03');
 
 --
 -- Índices para tabelas despejadas
@@ -97,6 +134,12 @@ INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
 --
 ALTER TABLE `autores`
   ADD PRIMARY KEY (`id_autor`);
+
+--
+-- Índices para tabela `contatos`
+--
+ALTER TABLE `contatos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices para tabela `livro`
@@ -126,25 +169,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `autores`
 --
 ALTER TABLE `autores`
-  MODIFY `id_autor` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_autor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de tabela `contatos`
+--
+ALTER TABLE `contatos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `livro`
 --
 ALTER TABLE `livro`
-  MODIFY `id_livro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_livro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT de tabela `livros`
 --
 ALTER TABLE `livros`
-  MODIFY `idloc` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `idloc` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Restrições para despejos de tabelas
